769e981b07302a627b0a3249e2fab5159f7e4039
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-04 22:24:02 -0600

    New components and installing React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import PatientForm from "./components/PatientForm"
+import PatientsList from "./components/PatientsList"
 
 function App() {
   
@@ -7,8 +9,13 @@ function App() {
         <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
           Seguimiento de pacientes {''}
           <span className="text-indigo-700">Veterinaria</span>
-
         </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm/>
+          <PatientsList/>
+        </div>
+
       </div>
     </>
   )
