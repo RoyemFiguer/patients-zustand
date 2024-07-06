d8f41d802685c50edc4b54fbbda06bfbdd1f9a5a
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-06 13:38:36 -0600

    Delete patients with Zustand

--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,7 @@ import { DraftPatient, Patient } from "./types"
 type PatientState = {
     patients: Patient[]
     addPatient: (data: DraftPatient) => void
+    deletePatient: (id: Patient['id']) => void
 }
 
 const createPatient = (patient: DraftPatient) : Patient => {
@@ -19,5 +20,10 @@ export const usePatientStore = create<PatientState>((set,get) => ({
             patients: [...state.patients, newPatient]
         }))
 
+    },
+    deletePatient: (id) => {
+       set((state) => ({
+        patients: state.patients.filter(patient => patient.id !== id)
+       }))
     }
 }))
