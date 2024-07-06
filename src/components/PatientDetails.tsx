9b51c67a2e38adead482210c5e9d88e5fc75b4e9
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-05 18:31:15 -0600

    Added buttons for editing and deleting

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -37,6 +37,20 @@ export default function PatientDetails({patient}: PatientDetailsProps) {
             label="Síntomas"
             data={patient.symptoms}
         />
+
+        <div className="flex justify-between gap-3 mt-10">
+            <button
+                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+            >
+                Editar
+            </button>
+
+            <button
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+            >
+                Eliminar
+            </button>
+        </div>
     </div>
   )
 }
